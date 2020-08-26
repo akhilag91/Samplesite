f25ca1b92e9a21251127919cdc397679bb295613
Akhila <akhila@tapchief.com>
2020-08-26 12:26:13 +0530

    static fix

--- storage_space/settings.py
+++ storage_space/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join('BASE_DIR', 'staticfiles')
+STATIC_URL = '/ourspace/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'ourspace/static'),
 )
 
 #
